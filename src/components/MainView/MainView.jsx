f73c042358f0c806a3279c05b62e19d2caf88a43
Aris Tchoukoualeu <38705757+Tchoukoualeu@users.noreply.github.com>
2021-09-10 15:47:12 +0200

    Update MainView.jsx

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -8,7 +8,8 @@ const MainView = () => {
   const [select, setSelect] = useState("");
 
   useEffect(() => {
-    fetch("https://restcountries.eu/rest/v2/all").then((res) => res.json())
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
       .then((res) => setCountries(res));
     );
   }, []);
